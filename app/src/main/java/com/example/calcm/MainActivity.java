8341fac5c93fac05234b9d66ff89468aa57dca7d
viniciusoliveiras <vinitag190@gmail.com>
2022-03-26 13:10:45 -0300

    login screen

---
+++ app/src/main/java/com/example/calcm/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.calcm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void signIn(View view){
+        Intent intent1 = new Intent(getApplicationContext(), Calculadora.class);
+        startActivity(intent1);
+    }
+}
